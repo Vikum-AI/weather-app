eae227b19e94f05220807667c5f51342173dfe1e
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-02-12 12:01:32 +0530

    Better display of the weather

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 city = input("Enter city name: ")
 request_url = f'{BASE_URL}?appid={API_KEY}&q={city}'
 response = requests.get(request_url)
-data = response.json() 
-print(data)  
 
+if response.status_code == 200: 
+	data = response.json() 
+	weather = data['weather'][0]['description']
+	temperature = round(data["main"]["temp"] - 273.15, 2) 
+	
+	print("Weather:", weather) 
+	print("Temperature:", temperature, "°C")
 
+else: 
+	print("Error fetching data") 
